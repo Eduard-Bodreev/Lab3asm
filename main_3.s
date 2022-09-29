d56381e7e9ee1fa11a43c9d2fe1f13e6c4f5c099
Eduard-Bodreev <70958954+Eduard-Bodreev@users.noreply.github.com>
2022-09-29 10:20:53 +0300

    Update main_3.s

--- main_3.s
+++ main_3.s
@@ -27,33 +27,33 @@ buffer:
     .global _start
     .type _start, %function
 _start:
-    mov x0, 1
+    mov x0, 1 // stdout
     adr x1, filename
     mov x2, filename_len
     mov x8, #64
-    svc #0 // output
+    svc #0 // вывести "Enter filename: " в stdout
 
     mov x0, 0 // stdin
     adr x1, buffer
     mov x2, buffer_cap
     mov x8, #63 // read
-    svc #0
+    svc #0  // ввести в buffer из stdin
     cbz x0, exit
 
     adr x1, buffer
-1: // find end of first line
-    cmp x0, 0
+1: // поиск конца первой строки
+    cmp x0, 0 // длина входной строки
     beq 2f
 
-    ldrb w7, [x1], 1
+    ldrb w7, [x1], 1 // w7 = *x1; x1 += 1
     cmp w7, '\n'
-    beq 2f
+    beq 2f // если w7 это конец строки то выйти
 
     sub x0, x0, 1
     b 1b
 2:
     sub x1, x1, 1
-    strb wzr, [x1], 1
+    strb wzr, [x1], 1 // поставить 0 вместо \n
     sub x0, x0, 1
 
     mov x20, x0
@@ -64,13 +64,13 @@ _start:
     mov x2, 1 | 0x40 | 0x200
     mov x3, 0b110110110
     mov x8, #56
-    svc #0
+    svc #0 // создать или открыть файл с именем как в buffer
 
-    mov x28, x0
+    mov x28, x0 // сохраняем дескриптор в x28
 
     cmp x0, xzr
     bge 1f
-    // error
+    // проверка ошибки открытия
     adr x1, file_error
     mov x2, file_error_len
     mov x0, #1
@@ -80,7 +80,7 @@ _start:
 1:
     
     adr x0, buffer
-2: // move remaining input to the start of the buffer
+2: // перемещение всего что было после первой строки в начало буффера
     cmp x20, 0
     beq 1f
 
@@ -93,25 +93,28 @@ _start:
     adr x1, buffer
     sub x0, x0, x1
 
-    mov x21, 1 // 0 if previous character was space
+    mov x21, 0 // 0 если предыдущий символ - пробел, иначе 1
 
 input_loop:
     cmp x0, 0
-    bne 1f // skip only the first time
+    bne 1f // в первый раз пропускаем ввод если до этого ввели больше одной строки
     mov x0, 0 // stdin
     adr x1, buffer
     mov x2, buffer_cap
     mov x8, #63 // read
-    svc #0
+    svc #0 // ввод в buffer из stdin
 1:
 
-    cbz x0, eof
-    // x0 = remaining length
-    adr x1, buffer // read pointer
-    adr x2, buffer // write pointer
+    cbz x0, eof // если введено 0 символов значит конец ввода
+    // x0 = оставшаяся длина введенных данных
+    adr x1, buffer // указатель чтения
+    adr x2, buffer // указатель записи
+    // читаем и пишем в один и тот же массив,
+    // но записывается всегда меньше символов чем было введено
 
 process_input:
-    ldrb w7, [x1], 1
+    ldrb w7, [x1], 1 // читаем по указателю чтения и увеличиваем
+    // проверяем пробельные символы
     cmp w7, ' '
     beq space
     cmp w7, '\t'
@@ -120,41 +123,42 @@ process_input:
     beq newline
     cmp w7, '\r'
     beq newline
+    // проверки не сработали - значит буква
 
     mov w6, 32
-    eor w7, w7, w6
-    mov x21, 1
+    eor w7, w7, w6 // w7 = w7 xor 32 -- смена регистра буквы
+    mov x21, 1 // предыдущий символ = буква
     b output
 
 space:
-    cbz x21, skip_char // if previous character was space
+    cbz x21, skip_char // пропускаем если предыдущий символ - пробел
 newline:
-    mov x21, 0
+    mov x21, 0 // предыдущий символ = пробел
 
 output:
-    strb w7, [x2], 1
+    strb w7, [x2], 1 // записываем по указателю записи и увеличиваем
     mov w8, w7
 
 skip_char:
     sub x0, x0, 1
     cmp x0, xzr
-    bgt process_input // if not all input was processed
+    bgt process_input // если остались входные данные, то обрабатываем их
 
     adr x1, buffer
-    sub x2, x2, x1 // output length
+    sub x2, x2, x1 // указатель записи - buffer = длина выводимых данных
     mov x0, x28
     mov x8, #64
-    svc #0 // output
+    svc #0 // вывод в файл
 
     mov x0, 0
-    b input_loop
+    b input_loop // продолжаем вводить данные пока они не закончатся
 eof:
 
-    mov x0, x28 // close file
+    mov x0, x28
     mov x8, #57
-    svc #0
+    svc #0 // закрываем файл
 exit:
-    mov x0, #0 // exit
+    mov x0, #0 // выход
     mov x8, #93
     svc #0
 
